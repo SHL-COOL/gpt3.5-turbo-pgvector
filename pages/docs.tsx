30feeb857c72e1794d019d6c1d889e95bfbad5f2
SHL-COOL <31879581+SHL-COOL@users.noreply.github.com>
2023-04-02 11:53:27 +0800

    Update docs.tsx

--- pages/docs.tsx
+++ pages/docs.tsx
@@ -188,7 +188,7 @@ const DocsPage: NextPage<Props> = ({ children, meta: pageMeta }: Props) => {
           </ResizablePanel>
 
           <div className="max-w-xl text-xs">
-            <p><sup>*</sup>服务将很快关闭，事实上，我目前只接受过以下文档的培训：</p>
+            <p><sup>*</sup>服务已经关闭，事实上，我目前只接受过以下文档的培训：</p>
             <ul>
               <li><a target="_blank" href="">https://iot3d.docs.ganweicloud.com</a></li>
             </ul>
